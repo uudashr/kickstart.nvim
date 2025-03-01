b8970ab37218575652e685eb0d0eb41efaeb164c
Nuruddin Ashr <uudashr@gmail.com>
2025-03-02 06:53:09 +0700

    Add zls

--- init.lua
+++ init.lua
@@ -660,6 +660,7 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         gopls = {},
+        zls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
