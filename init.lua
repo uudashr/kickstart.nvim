383e89c4fa34e210e19dd3fda7906b6268ae3288
Nuruddin Ashr <uudashr@gmail.com>
2025-03-04 18:08:34 +0700

    Configure gopls

--- init.lua
+++ init.lua
@@ -665,7 +665,10 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        gopls = {},
+        gopls = {
+          gofumpt = true,
+          usePlaceholders = true,
+        },
         zls = {},
         -- pyright = {},
         -- rust_analyzer = {},
