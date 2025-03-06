6d1ee5b05886fc8f387dc0a9253ee10b2d0e9153
Nuruddin Ashr <uudashr@gmail.com>
2025-03-07 00:21:51 +0700

    Add rust plugin

--- init.lua
+++ init.lua
@@ -671,7 +671,7 @@ require('lazy').setup({
         },
         zls = {},
         -- pyright = {},
-        rust_analyzer = {},
+        -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
