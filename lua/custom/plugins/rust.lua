85d99033863bf277db3718e3a694275a8d735e3e
Nuruddin Ashr <uudashr@gmail.com>
2025-03-07 11:47:28 +0700

    Add crates plugin

--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -1,5 +1,24 @@
 return {
-  'mrcjkb/rustaceanvim',
-  version = '^5', -- Recommended
-  lazy = false, -- This plugin is already lazy
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+  {
+    'Saecki/crates.nvim',
+    event = { 'BufRead Cargo.toml' },
+    opts = {
+      completion = {
+        crates = {
+          enabled = true,
+        },
+      },
+      lsp = {
+        enabled = true,
+        actions = true,
+        completion = true,
+        hover = true,
+      },
+    },
+  },
 }
